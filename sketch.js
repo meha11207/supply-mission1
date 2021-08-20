0b63288fc0a74d9f1d29d3ee7e2022be050c037e
meha11207 <79628242+meha11207@users.noreply.github.com>
2021-08-20 02:52:53 -0700

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,7 +1,5 @@
-var helicopterIMG, helicopterSprite;
-var packageSprite,packageIMG;
-var packageBody,ground;
-var line1,line2,line3;
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
@@ -11,17 +9,17 @@ function preload()
 {
 	helicopterIMG=loadImage("helicopter.png")
 	packageIMG=loadImage("package.png")
-	
 }
 
 function setup() {
 	createCanvas(800, 700);
 	rectMode(CENTER);
 	
+
 	packageSprite=createSprite(width/2, 80, 10,10);
 	packageSprite.addImage(packageIMG)
 	packageSprite.scale=0.2
-    
+
 	helicopterSprite=createSprite(width/2, 200, 10,10);
 	helicopterSprite.addImage(helicopterIMG)
 	helicopterSprite.scale=0.6
@@ -30,19 +28,38 @@ function setup() {
 	groundSprite.shapeColor=color(255)
 
 
-   
-    
-
 	engine = Engine.create();
 	world = engine.world;
 
-	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.1, isStatic:true});
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.4, isStatic:true});
 	World.add(world, packageBody);
 	
 
 	//Create a Ground
 	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
  	World.add(world, ground);
+
+ 	boxPosition=width/2-100
+ 	boxY=610;
+
+
+ 	boxleftSprite=createSprite(boxPosition, boxY, 20,100);
+ 	boxleftSprite.shapeColor=color(255,0,0);
+
+ 	boxLeftBody = Bodies.rectangle(boxPosition+20, boxY, 20,100 , {isStatic:true} );
+ 	World.add(world, boxLeftBody);
+
+ 	boxBase=createSprite(boxPosition+100, boxY+40, 200,20);
+ 	boxBase.shapeColor=color(255,0,0);
+
+ 	boxBottomBody = Bodies.rectangle(boxPosition+100, boxY+45-20, 200,20 , {isStatic:true} );
+ 	World.add(world, boxBottomBody);
+
+ 	boxleftSprite=createSprite(boxPosition+200 , boxY, 20,100);
+ 	boxleftSprite.shapeColor=color(255,0,0);
+
+ 	boxRightBody = Bodies.rectangle(boxPosition+200-20 , boxY, 20,100 , {isStatic:true} );
+ 	World.add(world, boxRightBody);
 
 
 	Engine.run(engine);
@@ -53,6 +70,7 @@ function setup() {
 function draw() {
   rectMode(CENTER);
   background(0);
+ 
   packageSprite.x= packageBody.position.x 
   packageSprite.y= packageBody.position.y 
   
@@ -61,10 +79,8 @@ function draw() {
 }
 
 function keyPressed() {
- if (keyCode == DOWN_ARROW) {
-   Matter.Body.setStatic(packageBody,false);
-  }
-}
-
-
-
+	if (keyCode == DOWN_ARROW) {
+	  Matter.Body.setStatic(packageBody,false);
+	 }
+   }
+   
